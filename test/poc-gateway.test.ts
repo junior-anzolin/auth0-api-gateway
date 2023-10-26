f1d98d8b88eb8887e429d0861e9ccefb28965843
Junior Anzolin <junioranzolin58@gmail.com>
2023-10-26 18:12:14 -0300

    Initial commit

---
+++ test/poc-gateway.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PocGateway from '../lib/poc-gateway-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/poc-gateway-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PocGateway.PocGatewayStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
